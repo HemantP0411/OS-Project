687909bf0e8025f64b6729bdc76ddbf9d421da9d
Hemant Pandey <44564579+HemantP0411@users.noreply.github.com>
2019-04-13 19:33:46 +0530

    update 1
    added sorting function to sort processes according to their arrival time

--- Os.c
+++ Os.c
@@ -2,9 +2,11 @@
 #include<conio.h>
 #include<stdlib.h>
 using namespace std;
+//variable declaration
+int arr[10],br[10],post[10],br1[10],wt[10],tarr[10],t1=3,t2=6,avgt=0,avgw=0,i,m,temp1,temp2,temp,j;
+//function to take user input of burst,arrival time of processes
 void input(int arrival[],int burst[],int burst1[])
 {
-	int i,m;
 	a:
 		system("cls");
 	printf("\nEnter number of processes");
@@ -13,6 +15,7 @@ void input(int arrival[],int burst[],int burst1[])
 	for(i=0;i<m;i++)
 	{  printf("\nfor processes %d :",i+1);
 		scanf("%d",&arrival[i]);
+		post[i]=i+1;
 	}
 	printf("\nenter burst time for processes:");
 	for(i=0;i<m;i++)
@@ -24,12 +27,11 @@ void input(int arrival[],int burst[],int burst1[])
 	system("cls");
 	printf("Is this your final table?\n\n");
 	printf("Process\tArrival Time\tBurst Time");
-	int j;
 	for(i=0;i<m;i++)
 	{
 		for(j=0;j<3;j++);
 		{
-			printf("\n  P%d    \t%d       \t%d",i+1,arrival[i],burst[i]);
+			printf("\n  P%d    \t%d       \t%d",post[i],arrival[i],burst[i]);
 		}
 	}
 	printf("\n\nIf no then press 'n' else press 'y'\n");
@@ -38,8 +40,44 @@ void input(int arrival[],int burst[],int burst1[])
 	if(ch=='n')
 	goto a;
 }
+//function for sorting according to arrival time
+void sort()
+{
+	for(j=0;j<m;j++)
+	{
+		for(i=0;i<m-1;i++)
+		{
+			if(arr[i]>arr[i+1])
+			{
+				
+				temp2=post[i];
+				post[i]=post[i+1];
+				post[i+1]=temp2;
+				
+				temp=arr[i];
+				arr[i]=arr[i+1];
+				arr[i+1]=temp;
+				
+				temp1=br[i];
+				br[i]=br[i+1];
+				br[i+1]=temp1;
+			}
+		}
+	}	
+	system("cls");
+	printf("\nFinal table after sorting according to arrival time\n\n");
+	printf("Process\tArrival Time\tBurst Time");
+	for(i=0;i<m;i++)
+	{
+		for(j=0;j<3;j++);
+		{
+			printf("\n  P%d    \t%d       \t%d",post[i],arr[i],br[i]);
+		}
+	}
+system("pause");
+}
 main()
 {
-	int arrival[10],burst[10],burst1[10],waiting[10],tarrival[10],t1=3,t2=6,avgtime=0,avgwait=0;
-	input(arrival,burst,burst1);
+	input(arr,br,br1);
+	sort();
 }
