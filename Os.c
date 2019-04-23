e6374e34372439e2f3631f8eaeaaaf3b071a2b58
Hemant Pandey <44564579+HemantP0411@users.noreply.github.com>
2019-04-24 00:29:52 +0530

    update 5
    (Inputs are given by default so respective function is disabled)
    what's new
    1>added new function for second iteration
    2>time quantum of second iteration is 6

--- Os.c
+++ Os.c
@@ -3,11 +3,11 @@
 #include<stdlib.h>
 using namespace std;
 //variable declaration
-int at[10],bt[10],post[10],tat[10],t1=3,t2=6,avgt=0,avgw=0,i,m,temp1,temp2,temp,j;
+int at[10]={0,2,4,13},bt[10]={18,23,13,10},post[10],t1=3,t2=6,i,m=4,temp1,temp2,temp,j;
 
-int ct[50]={0}, rqi[50]={0}, c=0, st, flg=0, tm=0, noe=0, pnt=0, btm[10], tt, wt,x;
+int ct[10]={0}, rqi[50]={0}, c=0, st, flg=0, tm=0, noe=0, pnt=0, btm[10]={18,23,13,10}, tt, wt[50]={0},x;
 float att, awt;
-int n;
+int n=4;
 //function to take user input of bt,at time of processes
 void input()
 {
@@ -44,7 +44,6 @@ void input()
 	if(ch=='n')
 	goto a;
 }
-//function for iteration 1 i.e. time quantum 3 unit
 void sort()
 {
 	for(j=0;j<m;j++)
@@ -65,7 +64,6 @@ void sort()
 				temp1=bt[i];
 				bt[i]=bt[i+1];
 				bt[i+1]=temp1;
-//				btm[i]=bt[i];
 			}
 		}
 	}	
@@ -81,17 +79,18 @@ void sort()
 	}
 system("pause");
 }
-void SearchStack01(int pnt,int tm){
+void searchst1(int pnt,int tm){
     for(int x=pnt+1;x<n;x++){
        if(at[x]<=tm){
           rqi[noe]=x+1;
           noe++;}
-    }//for
-}//void
+    }
+}
 
-void SearchStack02(int pnt, int tm){
-    for(int x=pnt+1;x<n;x++){
-       //---CheckQue
+void searchst2(int pnt, int tm)
+{
+    for(int x=pnt+1;x<n;x++)
+	{
        int fl=0;
        for(int y=0;y<noe;y++){
           if(rqi[y]==x+1){
@@ -99,71 +98,118 @@ void SearchStack02(int pnt, int tm){
        if(at[x]<=tm && fl==0 && btm[x]!=0){
           rqi[noe]=x+1;
           noe++;}
-    }//for
-}//void
+    }
+}
 
 void AddQue(int pnt){
     rqi[noe]=pnt+1;
     noe++;
-}//void
-
+}
+int start[10]={0},end[10]={0};
 void iteration1()
 {
 	n=m;
 	do{
     if(flg==0){
        st=at[0];
-       //---ReduceBT
+     
        if(btm[0]<=3){
           tm=st+btm[0];
           btm[0]=0;
-          SearchStack01(pnt,tm);}
+          searchst1(pnt,tm);}
        else{
           btm[0]=btm[0]-3;
           tm=st+3;
-          SearchStack01(pnt,tm);
+          searchst1(pnt,tm);
           AddQue(pnt);}
-    }//if
+    }
  
     else{
        pnt=rqi[0]-1;
        st=tm;
-       //---DeleteQue
+       
        for(x=0;x<noe && noe!=1;x++){
           rqi[x]=rqi[x+1];}
        noe--;
-       //---ReduceBT
+       
        if(btm[pnt]<=3){
           tm=st+btm[pnt];
           btm[pnt]=0;
-          SearchStack02(pnt, tm);}
+          searchst2(pnt, tm);}
        else{
          btm[pnt]=btm[pnt]-3;
           tm=st+3;
-          SearchStack02(pnt, tm);
+          searchst2(pnt, tm);
           AddQue(pnt);}
-    }//else
-
-    //AssignCTvalue
-    if(btm[pnt]==0){
+    }
+    
+    
        ct[pnt]=tm;
-    }//if
-
+       printf("%d %d %d\n",st,pnt,tm);
+       start[pnt]=st;
+       wt[pnt]+=((start[pnt]-end[pnt]));
+    end[pnt]=tm;
+    
    flg++;
-   printf("P%d-",pnt+1);
+   
+   	
 }while(noe!=0&&btm[n-1]==bt[n-1]);
-//printf("\n%d   %d",bt[m],btm[m]);
-for(x=0;x<n;x++)
+}
+int flag1=1,tm1=0,wt1[10];
+void waitf(int tm12,int index)
 {
-	printf("%d",btm[x]);
+		int in1;
+	for(in1=0;in1<m;in1++)
+	{
+		if(index!=in1)
+		{
+			wt1[i]+=tm12;
+		}
+	}
 }
+void iteration2()
+{
+	for(i=0;i<m;i++)
+	{
+		wt1[i]=wt[i]-at[i]+end[m-1]-end[i];
+	}
+	i=0;
+	while(flag1==1)
+	{
+		tm1=0;
+		if(btm[i]<=6)
+		{
+			tm1=btm[i];
+			btm[i]=0;
+		}
+		else
+		{
+			btm[i]-=6;
+			tm1=6;
+		}
+		waitf(tm1,i);
+	i++;
+	if(i==m)
+	{
+		flag1=0;
+	}
+	
+	}
 }
-
-
 main()
 {
-	input();
-	sort();
-//	printf("%d   %d",bt[m],btm[m]);
+	
+//	input();
+//	sort();
+
 	iteration1();
+	iteration2();
+printf("\n\nPROCESS\t AT\t BT\t CT\t TT\t WT\n");
+for(int x=0;x<n;x++){
+    tt=ct[x]-at[x];
+    printf("P%d \t %d \t %d \t %d \t %d \t %d\n",x+1,at[x],bt[x],ct[x],tt,wt1[x]);
+    awt=awt+wt[x];
+    att=att+tt;
+}
+
 }
