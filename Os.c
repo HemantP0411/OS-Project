1fae82168cf1e4bcc15cc63b67a75c7c1cf29216
Hemant Pandey <44564579+HemantP0411@users.noreply.github.com>
2019-04-25 18:35:19 +0530

    Final Project
    flow of program
    
    take user input
     |
    \/
    sort according to arrival time
     |
    \/
    run iteration1 with time quantum 3 until each process processes at least once
     |
    \/
    run iteration2 with time quantum 6 only once each process considering the completion of any process in between
     |
    \/
    sort according to available burst time
     |
    \/
    run iteration3 (fcfs) until each process completes it's processing
     |
    \/
    Display AT BT CT WT TAT and average Wt ,average TAT
     |
    \/
    Exit

--- Os.c
+++ Os.c
@@ -1,12 +1,10 @@
 #include<stdio.h>
-#include<conio.h>
 #include<stdlib.h>
-using namespace std;
 //variable declaration
-int at[10]={0,2,4,13},bt[10]={18,23,13,10},post[10],t1=3,t2=6,i,m=4,temp1,temp2,temp,temp12,temp11,j;
+int at[10],bt[10],post[10],t1=3,t2=6,i,m,temp1,temp2,temp,temp12,temp11,j;
 
-int ct[10]={0}, rqi[50]={0}, c=0, st, flg=0, tm=0, noe=0, pnt=0, btm[10]={18,23,13,10}, tt, wt[50]={0},x;
-float att, awt;
+int ct[10]={0}, rqi[50]={0}, c=0, st, flg=0, tm=0, noe=0, pnt=0, btm[10]={0}, tt, wt[50]={0},x,flag1=1,tm1=0,wt1[10],start[10]={0},end[10]={0};
+float att=0, awt=0;
 int n=4;
 //function to take user input of bt,at time of processes
 void input()
@@ -44,43 +42,10 @@ void input()
 	if(ch=='n')
 	goto a;
 }
-void sort()
+void searchst1(int pnt,int tm)
 {
-	for(j=0;j<m;j++)
-	{
-		for(i=0;i<m-j;i++)
-		{
-			if(at[i]>at[i+1])
-			{
-				
-				temp2=post[i];
-				post[i]=post[i+1];
-				post[i+1]=temp2;
-				
-				temp=at[i];
-				at[i]=at[i+1];
-				at[i+1]=temp;
-				
-				temp1=bt[i];
-				bt[i]=bt[i+1];
-				bt[i+1]=temp1;
-			}
-		}
-	}	
-	system("cls");
-	printf("\nFinal table after sorting according to at time\n\n");
-	printf("Process\tArrival Time\tBurst Time\n");
-	for(i=0;i<m;i++)
-	{
-		for(j=0;j<3;j++);
-		{
-			printf("  P%d    \t%d       \t%d\n",post[i],at[i],btm[i]);
-		}
-	}
-system("pause");
-}
-void searchst1(int pnt,int tm){
-    for(int x=pnt+1;x<n;x++){
+	int x;
+    for(x=pnt+1;x<n;x++){
        if(at[x]<=tm){
           rqi[noe]=x+1;
           noe++;}
@@ -88,22 +53,30 @@ void searchst1(int pnt,int tm){
 }
 void searchst2(int pnt, int tm)
 {  
-	for(int x=pnt+1;x<n;x++)
+	int x;
+	for(x=pnt+1;x<n;x++)
 	{
        int fl=0;
-       for(int y=0;y<noe;y++){
-          if(rqi[y]==x+1){
-             fl++;}}
+       int y;
+       for(y=0;y<noe;y++)
+	{
+      	if(rqi[y]==x+1)
+	  	{
+        	fl++;
+	  	}
+	}
+			 
        if(at[x]<=tm && fl==0 && btm[x]!=0){
           rqi[noe]=x+1;
           noe++;}
     }
+    
 }
-void AddQue(int pnt){
+void addqueue(int pnt)
+{
     rqi[noe]=pnt+1;
     noe++;
 }
-int start[10]={0},end[10]={0};
 void iteration1()
 {
 	n=m;
@@ -119,17 +92,14 @@ void iteration1()
           btm[0]=btm[0]-3;
           tm=st+3;
           searchst1(pnt,tm);
-          AddQue(pnt);}
+          addqueue(pnt);}
     }
- 
     else{
        pnt=rqi[0]-1;
-       st=tm;
-       
+       st=tm;  
        for(x=0;x<noe && noe!=1;x++){
           rqi[x]=rqi[x+1];}
        noe--;
-       
        if(btm[pnt]<=3){
           tm=st+btm[pnt];
           btm[pnt]=0;
@@ -138,21 +108,15 @@ void iteration1()
          btm[pnt]=btm[pnt]-3;
           tm=st+3;
           searchst2(pnt, tm);
-          AddQue(pnt);}
-    }
-    
-    
+          addqueue(pnt);}
+}
        ct[pnt]=tm;
        start[pnt]=st;
        wt[pnt]+=((start[pnt]-end[pnt]));
     end[pnt]=tm;
-    
-   flg++;
-   
-   	
+   flg++;  	
 }while(noe!=0&&btm[n-1]==bt[n-1]);
 }
-int flag1=1,tm1=0,wt1[10];
 void waitf(int tm12,int index)
 {
 		int in1;
@@ -193,8 +157,10 @@ void iteration2()
 	
 	}
 }
-void sortfcfs()
+void sort(int al)
 {
+	if(al==1)
+	{
 for(i=0;i<m;i++)
 {
 for(j=0;j<m-1;j++)
@@ -222,12 +188,47 @@ for(j=0;j<m-1;j++)
 				wt1[j+1]=temp11;	
 		}
 	}
-	}	
+	}
 }
-
+else
+{
+	for(j=0;j<m;j++)
+	{
+		for(i=0;i<m-1;i++)
+		{
+			if(at[i]>at[i+1])
+			{
+				
+				temp2=post[i];
+				post[i]=post[i+1];
+				post[i+1]=temp2;
+				
+				temp=at[i];
+				at[i]=at[i+1];
+				at[i+1]=temp;
+				
+				temp1=bt[i];
+				bt[i]=bt[i+1];
+				bt[i+1]=temp1;
+			}
+		}
+	}	
+	system("cls");
+	printf("\nFinal table after sorting according to at time\n\n");
+	printf("Process\tArrival Time\tBurst Time\n");
+	for(i=0;i<m;i++)
+	{
+		for(j=0;j<3;j++);
+		{
+			printf("  P%d    \t%d       \t%d\n",post[i],at[i],btm[i]);
+		}
+	}
+system("pause");
+}
+}
 void fcfs()
 {
-	sortfcfs();
+	sort(1);
 	i=0;
 	while(i<m)
 	{
@@ -239,22 +240,27 @@ void fcfs()
 		i++;
 	}
 }
+void display()
+{
+	system("cls");
+	printf("After completion of all processes (in order of their completion time)\n\n");
+	printf("\n\nPROCESS\t AT\t BT\t CT\t TAT\t WT\t\n");
+	int x;
+	for( x=0;x<n;x++)
+	{
+    tt=wt1[x]+bt[x];
+    printf("P%d \t %d \t %d \t %d \t %d \t %d\t\n",post[x],at[x],bt[x],tt+at[x],tt,wt1[x]);
+    awt=awt+wt1[x];
+    att=att+tt;
+	}
+	printf("\n\n Average waiting time is : %f\nAverage turn around time is : %f\n",awt/m,att/m);
+}
 main()
 {
-	
-//	input();
-//	sort();
-
+	input();
+	sort(0);
 	iteration1();
 	iteration2();
-//	sortfcfs();
 	fcfs();
-printf("\n\nPROCESS\t AT\t BT\t CT\t TT\t WT\t BTM\n");
-for(int x=0;x<n;x++){
-    tt=ct[x]-at[x];
-    printf("P%d \t %d \t %d \t %d \t %d \t %d\t %d\n",x+1,at[x],bt[x],ct[x],tt,wt1[x],btm[x]);
-    awt=awt+wt[x];
-    att=att+tt;
-}
-
+	display();
 }
